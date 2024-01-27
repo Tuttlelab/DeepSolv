33dc304d4da6ab43a110b12ca73da83c2fb9a3ca
avanteijlingen <57176642+avanteijlingen@users.noreply.github.com>
2024-01-27 23:09:26 +0000

    Update PoisonIvy.py

--- PoisonIvy.py
+++ PoisonIvy.py
@@ -9,7 +9,7 @@ import torch, tqdm
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.metrics import euclidean_distances, mean_squared_error
+from sklearn.metrics import euclidean_distances, mean_squared_error, mean_absolute_error
 from ase import Atoms
 import pandas, pickle
 
@@ -32,21 +32,60 @@ protons = {1: "H",
 inv_protons = {v: k for k, v in protons.items()}
 
 x = pKa()
-x.load_models("TrainDNN/models/", "best.pt")
+x.load_models("TrainDNN/models/cleaned", "best_L1.pt")
 #x.load_models("TrainDNN/model/", "best_L1.pt")
 print(x.Gmodels)
 assert "prot_aq" in x.Gmodels
 x.load_yates()
 x.use_yates_structures()
 
+state = "prot_aq"
+calculator = x.Gmodels[state]
 # =============================================================================
-# calculator = x.Gmodels["deprot_aq"]
 # h5 = h5py.File("BuildDataset/AqZ=0_rmsd=2.h5", 'r')
 # DS = h5py.File("BuildDataset/AqZ=0_rmsd=2_cleaned.h5", 'w')
 # 
 # =============================================================================
 
-calculator = x.Gmodels["prot_aq"]
+index = calculator.checkpointfiles
+index = [os.path.dirname(x.replace("TrainDNN/models/cleaned", "")) for x in index]
+result = pandas.DataFrame(index=index, columns=np.arange(1,12))
+for idx in range(1, 12):
+    mol = x.input_structures[idx][state]
+    calculator.mol = mol
+    calculator.MakeTensors()
+    pred = calculator.ProcessTensors(units="Ha", return_all=True)
+    #print(pred.shape)
+    result[idx] = pred.flatten()*627.5
+    x.yates_mols[idx][state]["DFT G"]
+    
+for idx in range(1, 12):
+    result.at["DFT", idx] = x.yates_mols[idx][state]["DFT G"]
+
+X = []
+Y = []
+Y2 = []
+for model in index:
+    RMSE = mean_squared_error(result.loc["DFT"], result.loc[model], squared=False)
+    MAE = mean_absolute_error(result.loc["DFT"], result.loc[model])
+    print(model, RMSE, MAE)
+    #result.loc["DFT"] - result.loc[model]
+    X.append(int(model.split("bs=")[1].split("_")[0]))
+    Y.append(RMSE)
+    Y2.append(MAE)
+plt.scatter(X, Y, label="RMSE")
+plt.scatter(X, Y2, label="MAE")
+plt.xlabel("batch_size")
+plt.ylabel("RMSE vs DFT opt (kcal/mol)")
+plt.title(state)
+plt.ylim(0, 10)
+plt.legend()
+fig = plt.gcf()
+fig.savefig(f"RMSE_vs_DFT_opt_{state}.png", dpi=300)
+
+
+sys.exit()
+
 h5 = h5py.File("BuildDataset/AqZ=1_rmsd=2.h5", 'r')
 DS = h5py.File("BuildDataset/AqZ=1_rmsd=2_cleaned.h5", 'w')
 
